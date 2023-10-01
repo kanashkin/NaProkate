e336e8bf8509c3ed70767b7c3fd6857972d52977
Alexander <kanashkin_ad@mail.ru>
2023-10-01 19:57:47 +0300

    commit

--- js/promo.js
+++ js/promo.js
@@ -1,23 +1,60 @@
-const closePromoPopup = () => {
-    const popUp = document.querySelector('.popup'),
+const selectMoscow = () => {
+    const popUpMoscow = document.querySelector('.popup'),
         popUpCloseBtns = document.querySelectorAll('.popup__button'),
         blurBlock = document.querySelector('.promo')
 
-    popUp.classList.add('active')
+    popUpMoscow.classList.add('active')
     blurBlock.classList.add('active')
 
     popUpCloseBtns.forEach(closeBtn => {
         closeBtn.addEventListener('click', () => {
+            popUpMoscow.classList.remove('active')
 
             if (closeBtn.getAttribute('data-button') === 'yes') {
-                popUp.classList.remove('active')
                 blurBlock.classList.remove('active')
+                writeCity('Москва')
             } else if (closeBtn.getAttribute('data-button') === 'no') {
-
+                selectCity()
             }
         })
         
     })
 }
 
-closePromoPopup()
+selectMoscow()
+
+const selectCity = () => {
+    const popUpCity = document.querySelector('.popup__city'),
+        backToPopup = document.querySelector('.popup__city-back'),
+        popupTabs = document.querySelectorAll('.popup__city-list-item'),
+        blurBlock = document.querySelector('.promo')
+
+    popUpCity.classList.add('active')
+
+    const back = () => {
+        backToPopup.addEventListener('click', () => {
+            popUpCity.classList.remove('active')
+            selectMoscow()
+        })
+    }
+
+    const tabs = () => {
+        popupTabs.forEach(item => {
+            item.addEventListener('click', () => {
+                const currentCity = item.textContent
+                writeCity(currentCity)
+                popUpCity.classList.remove('active')
+                blurBlock.classList.remove('active')
+            })
+        })
+    }
+
+    back()
+    tabs()
+}
+
+const writeCity = (cityName) => {
+    const cityField = document.querySelector('.header__city-name')
+
+    cityField.textContent = cityName
+}
