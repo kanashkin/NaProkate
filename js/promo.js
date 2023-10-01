dc7441c6312c7179c25021d0d09930ebb915d02e
Alexander <kanashkin_ad@mail.ru>
2023-10-01 17:08:31 +0300

    add catalog page

--- js/promo.js
+++ js/promo.js
@@ -1,17 +1,19 @@
 const closePromoPopup = () => {
     const popUp = document.querySelector('.popup'),
-    popUpCloseBtns = document.querySelectorAll('.popup__button'),
-    blurBlock = document.querySelector('.promo')
+        popUpCloseBtns = document.querySelectorAll('.popup__button'),
+        blurBlock = document.querySelector('.promo')
 
     popUpCloseBtns.forEach(closeBtn => {
         closeBtn.addEventListener('click', () => {
+
             if (closeBtn.getAttribute('data-button') === 'yes') {
                 popUp.classList.remove('active')
                 blurBlock.classList.remove('active')
             } else if (closeBtn.getAttribute('data-button') === 'no') {
-                
+
             }
         })
+        
     })
 }
 
