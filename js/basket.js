fcf92237380ba88d41932533279c79f697248172
Alexander <kanashkin_ad@mail.ru>
2023-10-02 22:21:40 +0300

    add calc func on basket page

--- js/basket.js
+++ js/basket.js
@@ -1,7 +1,25 @@
-const removeBtn = document.querySelectorAll('.basket-item-delete')
+const removeItem = () => {
+    const removeBtn = document.querySelectorAll('.basket-item-delete')
     
-removeBtn.forEach(item => {
-    item.addEventListener('click', () => {
-        item.parentNode.parentNode.remove()
+    removeBtn.forEach(item => {
+        item.addEventListener('click', () => {
+            item.parentNode.parentNode.remove()
+        })
     })
-})
+}
+
+const calc = () => {
+    const inputs = document.querySelectorAll('.basket-num')
+
+    inputs.forEach(item => {
+        item.addEventListener('change', () => {
+            const price = item.previousElementSibling.querySelector('span')
+            let total = item.nextElementSibling
+            
+            total.textContent = parseInt(price.textContent.replaceAll(' ', '')) * parseInt(item.value) + ' ₽'
+        })
+    })
+
+}
+calc()
+removeItem()
