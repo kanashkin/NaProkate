ad67b89f0423a3e7b1560cbf0b02f14f622b9a3e
Alexander <kanashkin_ad@mail.ru>
2023-10-10 19:19:54 +0300

    commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Попап с выбором Москвы
 const selectMoscow = () => {
     const popUpMoscow = document.querySelector('.popup'),
         popUpCloseBtns = document.querySelectorAll('.popup__button'),
@@ -26,7 +27,7 @@ const selectMoscow = () => {
     })
 }
 
-
+// Попап с выбором другого города
 const selectCity = () => {
     const popUpCity = document.querySelector('.popup__city'),
         backToPopup = document.querySelector('.popup__city-back'),
@@ -61,18 +62,16 @@ const selectCity = () => {
     tabs()
 }
 
-if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
-    if (localStorage.getItem('city') === null) {
+// Функция нажатия на триггер в поле с городом в хедере
+const changeCity = () => {
+    const selectCityTrigger = document.querySelector('.header__city-trigger')
+
+    selectCityTrigger.addEventListener('click', () => {
         selectMoscow()
-    }
+    })
 }
 
-const selectCityTrigger = document.querySelector('.header__city-trigger')
-
-selectCityTrigger.addEventListener('click', () => {
-    selectMoscow()
-})
-
+// Функция смены города в поле хедера
 const writeCity = () => {
     const cityField = document.querySelector('.header__city-name')
     if (localStorage.getItem('city') != null) {
@@ -80,8 +79,16 @@ const writeCity = () => {
     }
 }
 
-writeCity()
+// Проверка нахождения пользователя на главной страницы для попапа с выбором города
+const checkUserLocation = () => {
+    if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+        if (localStorage.getItem('city') === null) {
+            selectMoscow()
+        }
+    }
+}
 
+// Проверка: зарегистрирован ли пользователь для отображения блока перехода в ЛК в хедере
 const checkLogin = () => {
     const signUpBtn = document.querySelector('.sign-in-link')
     const userBlock = document.querySelector('.block-after-login')
@@ -97,4 +104,28 @@ const checkLogin = () => {
     }
 }
 
+//Проверка: зарегистрирован ли пользователь при переходе по ссылке в футере
+const checkFooterLogin = () => {
+    const footerLoginLinks = document.querySelectorAll('.footer-login')
+
+    footerLoginLinks.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault()
+            const itemHref = item.querySelector('a').getAttribute('href')
+
+            if (localStorage.getItem('logined') === 'login') {
+                window.location.href = 'account.html'
+            } else if (localStorage.getItem('logined') === 'unlogin') {
+                window.location.href = itemHref
+            }
+        })
+    })
+}
+
+selectMoscow()
+selectCity()
+changeCity()
+writeCity()
+checkUserLocation()
 checkLogin()
+checkFooterLogin()
