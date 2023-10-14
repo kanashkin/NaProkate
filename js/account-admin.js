ecad8153b4d8239b5614289d5d778fb5409f4f33
Artem <artem256korolev@gmail.com>
2023-10-14 17:47:26 +0300

    add page for admin

---
+++ js/account-admin.js
@@ -0,0 +1,31 @@
+const activeTab = document.querySelectorAll('.header__account__list-item')
+
+    activeTab[0].classList.add('active')
+
+const dropDownAdmin = () => {
+    const activeDropdown = document.querySelector('.filter-city')
+    const block = document.querySelectorAll('.admin__dropdown-item')
+    const selectText = document.querySelectorAll('.admin__dropdown-select')
+    const arrow = document.querySelectorAll('.account__admin-btn-arrow')
+
+    activeDropdown.addEventListener('click', () => {
+        block.forEach(open => {
+            open.classList.toggle('active')
+        })
+
+        arrow.forEach(item => {
+            item.classList.toggle('active')
+        })
+    })
+
+    
+
+    selectText.forEach(item => {
+        item.addEventListener('click', () => {
+            let activeText = document.querySelector('.admin__dropdown-active')
+            activeText.textContent = item.textContent
+        })
+    })
+}
+
+dropDownAdmin()
