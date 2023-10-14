443695739eca9ce6ae6b79e37352ff865d2fa415
Artem <artem256korolev@gmail.com>
2023-10-14 21:15:05 +0300

    commit

--- js/account-admin.js
+++ js/account-admin.js
@@ -29,3 +29,24 @@ const dropDownAdmin = () => {
 }
 
 dropDownAdmin()
+
+const accountAdminLink = () => {
+    const textLink = document.querySelectorAll('.account__table-link')
+    const blockActive = document.querySelectorAll('.distributors__account')
+
+    textLink[0].classList.add('active')
+    blockActive[0].classList.add('active')
+
+
+    textLink.forEach(item => {
+        item.addEventListener('click', () => {
+            item.classList.add('active')
+
+            blockActive.forEach(item => {
+                item.classList.add('active')
+            })
+        })
+    })
+}
+
+accountAdminLink()
